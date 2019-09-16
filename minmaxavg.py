bf018848ee7c230f673f3a3455905f3b1344f3be
root-kiskacsa <deejay.spory@gmail.com>
2019-09-16 10:04:37 +0200

    wow

---
+++ minmaxavg.py
@@ -0,0 +1,23 @@
+list = []
+a = int(input("Meddig?\n"))
+#if a <= 1:
+#	print("1 él azért nagyobb számot adj")
+
+sum = 0
+avg = 0.0
+while len(list) != a:
+	list.append(int(input("Szám:")))
+print()
+
+for x in range(len(list)):
+	min = list[0]
+	max = list[0]
+	sum += list[x]
+	if list[x] < min:
+		min = list[x]
+	if list[x] > max:
+		max = list[x]
+	print(x+1,".szám:",list[x])
+
+avg = sum / len(list)
+print("\nA megadott számok közül a legkisebb:",min,"legnagyobb:",max,"és az átlaguk:",avg)
